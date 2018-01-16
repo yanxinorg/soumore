6407327f858080a20bdbf94b4698585677867dc9
ChenXianSen <15951982354@163.com>
2018-01-16 17:56:51 +0800

    update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -26,14 +26,14 @@
                             <img alt="image" class="img-circle" src="{{ asset('back/admin/img/profile_small.jpg') }}" />
                              </span>
                             <a data-toggle="dropdown" class="dropdown-toggle" href="index.html#">
-                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">David Williams</strong>
-                             </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
+                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">陈先森</strong>
+                             </span> <span class="text-muted text-xs block">info<b class="caret"></b></span> </span> </a>
                             <ul class="dropdown-menu animated fadeInRight m-t-xs">
                                 <li><a href="profile.html">Profile</a></li>
                                 <li><a href="contacts.html">Contacts</a></li>
                                 <li><a href="mailbox.html">Mailbox</a></li>
                                 <li class="divider"></li>
-                                <li><a href="login.html">Logout</a></li>
+                                <li><a href="login.html">登出</a></li>
                             </ul>
                         </div>
                         <div class="logo-element">
@@ -41,7 +41,7 @@
                         </div>
                     </li>
                     <li class="active">
-                        <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboards</span> <span class="fa arrow"></span></a>
+                        <a href="index.html"><i class="fa fa-windows"></i> <span class="nav-label">控制台</span> <span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li class="active"><a href="index.html">Dashboard v.1</a></li>
                             <li><a href="dashboard_2.html">Dashboard v.2</a></li>
@@ -50,15 +50,26 @@
                             <li><a href="dashboard_5.html">Dashboard v.5 <span class="label label-primary pull-right">NEW</span></a></li>
                         </ul>
                     </li>
-                    <li>
-                        <a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Layouts</span></a>
+                    
+                    
+<!--                     RBAC权限管理	 -->
+                     <li>
+                        <a href="index.html#"><i class="fa fa-bars"></i> <span class="nav-label">权限管理</span><span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                        	<li><a href="index.html"><span class="nav-label">路由</span></a></li>
+                            <li><a href="index.html"><span class="nav-label">用户</span></a></li>
+                            <li><a href="index.html"><span class="nav-label">角色</span></a></li>
+                            <li><a href="index.html"><span class="nav-label">权限</span></a></li>
+                        </ul>
                     </li>
+                    
+<!--                    内容管理 -->
                     <li>
-                        <a href="index.html#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Graphs</span><span class="fa arrow"></span></a>
+                        <a href="index.html#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">内容管理</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse">
-                            <li><a href="graph_flot.html">Flot Charts</a></li>
-                            <li><a href="graph_morris.html">Morris.js Charts</a></li>
-                            <li><a href="graph_rickshaw.html">Rickshaw Charts</a></li>
+                            <li><a href="graph_flot.html">分类管理</a></li>
+                            <li><a href="graph_morris.html">文章管理</a></li>
+                            <li><a href="graph_rickshaw.html">话题管理</a></li>
                             <li><a href="graph_chartjs.html">Chart.js</a></li>
                             <li><a href="graph_chartist.html">Chartist</a></li>
                             <li><a href="c3.html">c3 charts</a></li>
