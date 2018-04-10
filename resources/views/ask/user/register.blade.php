926a37bf1d72b1180c60544f8c247c67662db6a2
ChenXianSen <15951982354@163.com>
2018-04-10 16:48:33 +0800

    update

--- resources/views/ask/user/register.blade.php
+++ resources/views/ask/user/register.blade.php
@@ -15,8 +15,6 @@
 <script src="{{ asset('ask/register_files/jquery.form.js') }}" type="text/javascript"></script>
 <script src="{{ asset('ask/register_files/plug-in_module.js') }}" type="text/javascript"></script>
 <script src="{{ asset('ask/register_files/aws.js') }}" type="text/javascript"></script>
-<script src="{{ asset('ask/register_files/aw_template.js') }}" type="text/javascript"></script>
-<script src="{{ asset('ask/register_files/app.js') }}" type="text/javascript"></script>
 <script type="text/javascript" src="{{ asset('ask/register_files/compatibility.js') }}"></script>
 <body screen_capture_injected="true">
 <div class="aw-register-box">
@@ -25,37 +23,36 @@
         <h1>注册新用户</h1>
     </div>
     <div class="mod-body">
-        <form class="aw-register-form" action="" method="post" id="register_form">
+        <form class="aw-register-form" action="{{ url('/register') }}"  method="post" id="register">
+            {{ csrf_field() }}
             <ul>
                 <li class="alert alert-danger collapse error_message text-left">
                     <i class="icon icon-delete"></i> <em></em>
                 </li>
                 
                 <li>
-                    <input class="aw-register-name form-control" type="text" name="user_name" placeholder="用户名" >
+                    <input type="text" id="username" name="username" value="{{ old('username') }}" placeholder="用户名" class="aw-register-name form-control">
+                </li>
+                
+                <li  class="aw-register-verify">
+                    <a class="sendEmail btn btn-default btn-blue pull-right"  id="second" style="background-color:#66B7FF;color:white;">　　　发送　　　</a>
+                	<input class="sendInput form-control" type="text"  id="email" name="email" value="{{ old('email') }}" placeholder="邮箱" >
+                </li>
+                
+                <li >
+                    <input type="text" name="captcha" value="{{ old('captcha') }}" placeholder="邮箱验证码" class="form-control">
                 </li>
                 
                 <li>
-                	<input type="text"  id="email" name="email" value="{{ old('email') }}" placeholder="邮箱" class="sendInput form-control">
-                  	<a class="sendEmail btn btn-primary" id="second" value="">发送</a>
-<!--                     <input class="aw-register-email form-control" type="text" name="email" placeholder="邮箱"  > -->
-                </li>
-                
-                <li class="aw-register-verify">
-                    <img class="pull-right" id="captcha" src="..">
-                    <input type="text" class="form-control" name="seccode_verify" placeholder="验证码">
-                </li>
-                
-                <li>
-                    <input class="aw-register-pwd form-control" type="password" name="password" placeholder="密码" >
+                    <input type="password" name="password" value="{{ old('password') }}" id="password" placeholder="密码" class="aw-register-pwd form-control">
                 </li>
                
                 <li>
-                    <input class="aw-register-pwd form-control" type="password" name="repassword" placeholder="重复密码" >
+                    <input type="password" name="password_confirmation" value="{{ old('password_confirmation') }}" placeholder="重复密码" class="aw-register-pwd form-control">
                 </li>
                
                 <li class="clearfix">
-                    <button class="btn btn-large btn-blue btn-block" onclick="AWS.ajax_post($(&#39;#register_form&#39;), AWS.ajax_processer, &#39;error_message&#39;); return false;">注册</button>
+                    <button type="submit" class="btn btn-large btn-blue btn-block">注册</button>
                 </li>
             </ul>
         </form>
@@ -75,6 +72,135 @@
 <a class="aw-back-top hidden-xs" href="javascript:;" onclick="$.scrollTo(1, 600, {queue:true});"><i class="icon icon-up"></i></a>
 <!-- DO NOT REMOVE -->
 <div id="aw-ajax-box" class="aw-ajax-box"></div>
-<div style="display:none;" id="__crond"><img src="./register_files/saved_resource(1)" width="1" height="1"></div>
-<!-- Escape time: 0.033930063247681 --><!-- / DO NOT REMOVE -->
+<!-- 弹层 -->
+<script src="{{ asset('ask/layer/layer.js') }}"></script>
+<script src="{{ asset('ask/common/js/jquery.cookie.js') }}"></script>
+<script type="text/javascript">
+    //发送验证码时添加cookie
+    function addCookie(name,value,expiresHours)
+    {
+//判断是否设置过期时间,0代表关闭浏览器时失效
+        if(expiresHours>0)
+        {
+            var date = new Date();
+            date.setTime(date.getTime() + expiresHours*1000);
+            $.cookie(name, escape(value), {expires: date});
+        }else{
+            $.cookie(name, escape(value));
+        }
+    }
+    //修改cookie的值
+    function editCookie(name,value,expiresHours)
+    {
+        if(expiresHours>0){
+            var date=new Date();
+            date.setTime(date.getTime()+expiresHours*1000);
+            //单位是毫秒
+            $.cookie(name, escape(value), {expires: date});
+        }else{
+            $.cookie(name, escape(value));
+        }
+    }
+    //根据名字获取cookie的值
+    function getCookieValue(name)
+    {
+        return $.cookie(name);
+    }
+
+    $(function()
+    {
+        $("#second").click(function ()
+        {
+            sendCode($("#second"));
+        });
+        //获取cookie值
+        v = getCookieValue("secondsremained");
+        if(v>0){
+            //开始倒计时
+            settime($("#second"));
+        }
+    });
+    //发送验证码
+    function sendCode(obj)
+    {
+        var email = $("#email").val();
+        var result = isEmail(email);
+        if(result)
+        {
+            //进行ajax调用
+            $.post("{{ url('/email/captcha') }}",
+                    {
+                        "_token":'{{ csrf_token() }}',
+                        "email": email,
+                    },function(data){
+                        var datas = JSON.parse(data);
+                        if(!datas.Success)
+                        {
+                            layer.msg(datas.Msg);
+                            return;
+                        }else{
+                            layer.msg(datas.Msg);
+                            //添加cookie记录,有效时间60s
+                            addCookie("secondsremained",60,60);
+                            //开始倒计时
+                            settime(obj);
+                        }
+                    });
+        }
+    }
+    var countdown;
+    function settime(obj)
+    {
+        countdown = getCookieValue("secondsremained");
+        if(countdown == undefined)
+        {
+            obj.removeAttr("disabled");
+            obj.text("　　　发送　　　");
+            return;
+        }else{
+            obj.attr("disabled",true);
+            obj.text("　　　"+countdown+ "秒　　　");
+            countdown--;
+            editCookie("secondsremained",countdown,countdown+1);
+        }
+        //每1000毫秒执行一次
+        setTimeout(function() {
+            settime(obj);
+        },1000)
+    }
+    //校验邮箱是否合法
+    function isEmail()
+    {
+        var email = $("#email").val();
+        var myreg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+        if(!myreg.test(email))
+        {
+            layer.msg('请输入有效的邮箱地址');
+            return false;
+        }else{
+            return true;
+        }
+    }
+    //检测用户是否已经存在
+    $("#username").change(function(){
+        var username = $("#username").val();
+        //进行ajax调用
+        $.post("{{ url('/user/check') }}",
+                {
+                    "_token":'{{ csrf_token() }}',
+                    "username": username,
+                },function(data){
+                    var datas = JSON.parse(data);
+                    if(!datas.Success && datas.Code == 201)
+                    {
+                        layer.msg(datas.Msg);
+                        return;
+                    }else if(!datas.Success && datas.Code == 204)
+                    {
+                        layer.msg(datas.Msg);
+                        return;
+                    }
+                });
+    });
+</script>
 </body></html>
