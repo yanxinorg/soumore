4604e04bf43a92ed451956c491e37cbeea6258f0
ChenXianSen <15951982354@163.com>
2018-05-22 18:01:12 +0800

    update

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
     //CMS首页
     public function index()
     {
-    	return view('admin.index.index');
+        $os = [];
+        //laravel版本
+        $laravel = app();
+        $os['laravel_ver'] = $laravel::VERSION;
+        //数据库版本
+        $os['mysql_ver'] = '';
+        //服务器系统当前时间
+        $os['current_time'] = time();
+    	return view('admin.index.index',['os'=>$os]);
     }
 }
--- resources/views/admin/index/index.blade.php
+++ resources/views/admin/index/index.blade.php
@@ -9,75 +9,38 @@
                         <div class="ibox-content no-padding">
                             <ul class="list-group">
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REDIRECT_MIBDIRS:</a>    "D:/xampp/php/extras/mibs"</p>
+                                    <p><h4>基础环境</h4></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REDIRECT_MYSQL_HOME:</a> "\xampp\mysql\bin"</p>
+                                    <p style="font-size:16px;"><span >Php版本：</span>&nbsp;<span class="text-info"><?php echo PHP_VERSION; ?> </span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REDIRECT_OPENSSL_CONF:</a>   "D:/xampp/apache/bin/openssl.cnf"</p>
-                                </li>
-                                <li class="list-group-item ">
-                                    <p><a class="text-info" href="index.html#">REDIRECT_PHP_PEAR_SYSCONF_DIR:</a>   "\xampp\php"</p>
+                                    <p style="font-size:16px;"><span >Laravel版本：</span>&nbsp;<span class="text-info">{{ $os['laravel_ver'] }}</span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REDIRECT_PHPRC:</a>  "\xampp\php"</p>
+                                    <p style="font-size:16px;"><span >服务器操作系统：</span>&nbsp;<span class="text-info"><?php echo PHP_OS; ?></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REDIRECT_TMP:</a>    "\xampp\tmp"</p>
+                                    <p style="font-size:16px;"><span >服务器端信息：</span>&nbsp;<span class="text-info"><?php echo $_SERVER ['SERVER_SOFTWARE']; ?></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REDIRECT_STATUS:</a> "200"</p>
+                                    <p style="font-size:16px;"><span >最大上传限制：</span>&nbsp;<span class="text-info"><?PHP echo get_cfg_var("upload_max_filesize")?get_cfg_var("upload_max_filesize"):"不允许上传附件"; ?></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">MIBDIRS:</a> "D:/xampp/php/extras/mibs"</p>
+                                    <p style="font-size:16px;"><span >最大执行时间：</span>&nbsp;<span class="text-info"><?PHP echo get_cfg_var("max_execution_time")."秒 "; ?></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">MYSQL_HOME:</a>  "\xampp\mysql\bin"</p>
+                                    <p style="font-size:16px;"><span >Mysql数据库的版本：</span>&nbsp;<span class="text-info"></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">OPENSSL_CONF:</a>    "D:/xampp/apache/bin/openssl.cnf"</p>
+                                    <p style="font-size:16px;"><span >MySQL最大连接数：</span>&nbsp;<span class="text-info"><?php echo @get_cfg_var("mysql.max_links")==-1 ? "不限" :@get_cfg_var("mysql.max_links");?></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">PHP_PEAR_SYSCONF_DIR:</a>    "\xampp\php"</p>
+                                    <p style="font-size:16px;"><span >脚本运行占用最大内存：</span>&nbsp;<span class="text-info"><?php echo get_cfg_var ("memory_limit")?get_cfg_var("memory_limit"):"无" ?></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">PHPRC:</a>   "\xampp\php"</p>
+                                    <p style="font-size:16px;"><span >服务器系统时间：</span>&nbsp;<span class="text-info">{{ date("Y-m-d G:i:s",$os['current_time']) }}</span></p>
                                 </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">TMP:</a> "\xampp\tmp"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">HTTP_HOST:</a>   "soumore.com"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">HTTP_CONNECTION:</a> "keep-alive"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">HTTP_UPGRADE_INSECURE_REQUESTS:</a>  "1"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">HTTP_USER_AGENT:</a> "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">HTTP_ACCEPT:</a> "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">HTTP_REFERER:</a>    "http://soumore.com/person/pass"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">HTTP_ACCEPT_ENCODING:</a>    "gzip, deflate"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">HTTP_ACCEPT_LANGUAGE:</a>    "zh-CN,zh;q=0.9"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">PATH:</a>    "C:\Python27\;C:\Python27\Scripts;C:\Ruby23-x64\bin;%APPCAN_PATH%;C:\Program Files (x86)\Common Files\NetSarang;C:\Windows\system32;C:\Windows;C:\Windows\System3 ▶"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">SystemRoot:</a>  "C:\Windows\system32\cmd.exe"</p>
-                                </li>
-
                             </ul>
                         </div>
                     </div>
@@ -87,76 +50,34 @@
                         <div class="ibox-content no-padding">
                             <ul class="list-group">
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">PATHEXT:</a> ".COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC"</p>
+                                    <p><h4>运行环境</h4></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">WINDIR:</a>  "<address>Apache/2.4.23 (Win32) OpenSSL/1.0.2h PHP/7.0.9 Server at soumore.com Port 80</address>\n"</p>
+                                    <p style="font-size:16px;"><span >Php版本：</span>&nbsp;<span class="text-info"><?php echo PHP_VERSION; ?> </span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">SERVER_SOFTWARE:</a> "Apache/2.4.23 (Win32) OpenSSL/1.0.2h PHP/7.0.9"</p>
+                                    <p style="font-size:16px;"><span >Laravel版本：</span>&nbsp;<span class="text-info">{{ $os['laravel_ver'] }}</span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">SERVER_NAME:</a> "soumore.com"</p>
+                                    <p style="font-size:16px;"><span >服务器操作系统：</span>&nbsp;<span class="text-info"><?php echo PHP_OS; ?></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">SERVER_ADDR:</a> "127.0.0.1"</p>
+                                    <p style="font-size:16px;"><span >服务器端信息：</span>&nbsp;<span class="text-info"><?php echo $_SERVER ['SERVER_SOFTWARE']; ?></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">SERVER_PORT:</a> "80"</p>
+                                    <p style="font-size:16px;"><span >最大上传限制：</span>&nbsp;<span class="text-info"><?PHP echo get_cfg_var("upload_max_filesize")?get_cfg_var("upload_max_filesize"):"不允许上传附件"; ?></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REMOTE_ADDR:</a> "127.0.0.1"</p>
+                                    <p style="font-size:16px;"><span >最大执行时间：</span>&nbsp;<span class="text-info"><?PHP echo get_cfg_var("max_execution_time")."秒 "; ?></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">DOCUMENT_ROOT:</a>   "D:/xampp/htdocs/SouMore/soumore/public"</p>
+                                    <p style="font-size:16px;"><span >MYSQL数据库的版本：</span>&nbsp;<span class="text-info"></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REQUEST_SCHEME:</a>  "http"</p>
+                                    <p style="font-size:16px;"><span >脚本运行占用最大内存：</span>&nbsp;<span class="text-info"><?PHP echo get_cfg_var ("memory_limit")?get_cfg_var("memory_limit"):"无" ?></span></p>
                                 </li>
                                 <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">CONTEXT_PREFIX:</a>  ""</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">CONTEXT_DOCUMENT_ROOT:</a>   "D:/xampp/htdocs/SouMore/soumore/public"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">SERVER_ADMIN:</a>    "root@soumore.com"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">SCRIPT_FILENAME:</a> "D:/xampp/htdocs/SouMore/soumore/public/index.php"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REMOTE_PORT:</a> "64565"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REDIRECT_URL:</a>    "/person/thumb"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">GATEWAY_INTERFACE:</a>   "CGI/1.1"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">SERVER_PROTOCOL:</a> "HTTP/1.1"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REQUEST_METHOD:</a>  "GET"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">QUERY_STRING:</a>    ""</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REQUEST_URI:</a> "/person/thumb"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">SCRIPT_NAME:</a> "/index.php"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">PHP_SELF:</a>    "/index.php"</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REQUEST_TIME_FLOAT:</a>  1526526431.689</p>
-                                </li>
-                                <li class="list-group-item">
-                                    <p><a class="text-info" href="index.html#">REQUEST_TIME:</a>    1526526431</p>
+                                    <p style="font-size:16px;"><span >服务器系统时间：</span>&nbsp;<span class="text-info">{{ date("Y-m-d G:i:s",$os['current_time']) }}</span></p>
                                 </li>
                             </ul>
                         </div>
